76abb65500d20608fd32e69e831f758dd22b4917
Ramon Cruz <ramon.cruz.comercial@gmail.com>
2018-06-14 22:25:20 -0300

    ajustes

--- src/pages/Video.js
+++ src/pages/Video.js
@@ -14,9 +14,9 @@ export default class Video extends React.Component{
         <ScrollView>
                 <YouTube
                     videoId="Uhf7i-huOVo"   // The YouTube video ID
-                    play={true}             // control playback of video with true/false
-                    fullscreen={true}       // control whether the video should play in fullscreen or inline
-                    loop={true}             // control whether the video should loop when ended
+                    play={false}             // control playback of video with true/false
+                    fullscreen={false}       // control whether the video should play in fullscreen or inline
+                    loop={false}             // control whether the video should loop when ended
                     apiKey='AIzaSyAeO2WDeUoU7oFmPsbd1MMRI_aHbFdkFDQ'
                     onReady={e => this.setState({ isReady: true })}
                     onChangeState={e => this.setState({ status: e.state })}
@@ -25,45 +25,7 @@ export default class Video extends React.Component{
 
                     style={{ alignSelf: 'stretch', height: 300 ,padding:30}}
                 />
-                <YouTube
-                    videoId="Uhf7i-huOVo"   // The YouTube video ID
-                    play={true}             // control playback of video with true/false
-                    fullscreen={true}       // control whether the video should play in fullscreen or inline
-                    loop={true}             // control whether the video should loop when ended
-                    apiKey='AIzaSyAeO2WDeUoU7oFmPsbd1MMRI_aHbFdkFDQ'
-                    onReady={e => this.setState({ isReady: true })}
-                    onChangeState={e => this.setState({ status: e.state })}
-                    onChangeQuality={e => this.setState({ quality: e.quality })}
-                    onError={e => this.setState({ error: e.error })}
-
-                    style={{ alignSelf: 'stretch', height: 300 ,padding:30}}
-                />
-                <YouTube
-                    videoId="Uhf7i-huOVo"   // The YouTube video ID
-                    play={true}             // control playback of video with true/false
-                    fullscreen={true}       // control whether the video should play in fullscreen or inline
-                    loop={true}             // control whether the video should loop when ended
-                    apiKey='AIzaSyAeO2WDeUoU7oFmPsbd1MMRI_aHbFdkFDQ'
-                    onReady={e => this.setState({ isReady: true })}
-                    onChangeState={e => this.setState({ status: e.state })}
-                    onChangeQuality={e => this.setState({ quality: e.quality })}
-                    onError={e => this.setState({ error: e.error })}
-
-                    style={{ alignSelf: 'stretch', height: 300 ,padding:30}}
-                />
-                <YouTube
-                    videoId="Uhf7i-huOVo"   // The YouTube video ID
-                    play={true}             // control playback of video with true/false
-                    fullscreen={true}       // control whether the video should play in fullscreen or inline
-                    loop={true}             // control whether the video should loop when ended
-                    apiKey='AIzaSyAeO2WDeUoU7oFmPsbd1MMRI_aHbFdkFDQ'
-                    onReady={e => this.setState({ isReady: true })}
-                    onChangeState={e => this.setState({ status: e.state })}
-                    onChangeQuality={e => this.setState({ quality: e.quality })}
-                    onError={e => this.setState({ error: e.error })}
-
-                    style={{ alignSelf: 'stretch', height: 300 ,padding:30}}
-                />
+                
         </ScrollView>
         );
     }
